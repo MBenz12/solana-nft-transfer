2643fc6c420aa06c7e738cc03ba3b331d454d609
mbenz12 <mbenz10120112@gmail.com>
2024-07-24 09:45:21 -0400

    updated mainnet rpc url

--- app/components/WalletProvider.tsx
+++ app/components/WalletProvider.tsx
@@ -4,11 +4,11 @@ import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
+// import { clusterApiUrl } from '@solana/web3.js';
 
 export const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const network = WalletAdapterNetwork.Mainnet;
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    const endpoint = useMemo(() => "https://mainnet.helius-rpc.com/?api-key=147a018f-8db1-4994-b3c1-eb8886e37bc2", []);
 
     const wallets = useMemo(
         () => [
